45b8148bca2ad680d6d3d8dee7ead5ca57c9e1ab
Muizzuddin-github <muizzuddin334@gmail.com>
2025-08-20 14:44:35 +0700

    contact

---
+++ src/components/contact.js
@@ -0,0 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
+const Contact = () => {
+  return (
+    <section className="p-5">
+      <section>
+        <section className="flex">
+          <i className="fa-solid fa-location-dot text-4xl"></i>
+          <section className="">
+            <p>7882 Longbranch Rd</p>
+            <p>Wooster, New York</p>
+          </section>
+        </section>
+      </section>
+    </section>
+  );
+};
+
+export default Contact;
