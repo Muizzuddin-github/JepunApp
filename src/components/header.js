d83550d8995a12ffac8acd22d28fdc56f3230426
Muizzuddin-github <muizzuddin334@gmail.com>
2025-08-21 13:56:58 +0700

    fix ui contact, navbar dan header

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,23 @@
+import Link from "next/link";
+
 const Header = () => {
   return (
     <section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
       <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
         <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
-          We invest in the worlds potential
+          Homestay
         </h1>
         <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
-          Here at Flowbite we focus on markets where technology, innovation, and
-          capital can unlock long-term value and drive economic growth.
+          Tempat nyaman dan tenang, cocok untuk liburan atau perjalanan bisnis.
+          Lokasi strategis, fasilitas lengkap, dan suasana seperti di rumah
+          sendiri.
         </p>
-        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-          <a
-            href="#"
-            className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 px-5">
+          <Link
+            href="/"
+            className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 "
           >
-            Get started
+            Jelajahi Sekarang
             <svg
               className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
               aria-hidden="true"
@@ -30,13 +33,7 @@ const Header = () => {
                 d="M1 5h12m0 0L9 1m4 4L9 9"
               />
             </svg>
-          </a>
-          <a
-            href="#"
-            className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
-          >
-            Learn more
-          </a>
+          </Link>
         </div>
       </div>
     </section>
