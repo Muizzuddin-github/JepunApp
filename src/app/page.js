9fcf9e2a6404a41210ea099e0537f2cd3cdde4aa
Muizzuddin-github <muizzuddin334@gmail.com>
2025-08-21 15:06:42 +0700

    fasilitas

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Contact from "@/components/contact";
 import Header from "@/components/header";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import Fasilitas from "@/components/fasilitas";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Contact />
       <Navbar />
       <Header />
+      <Fasilitas />
       <Footer />
     </>
   );
