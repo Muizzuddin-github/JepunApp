6232ca83373daaa0ce69cd6cfc33fcf1de9fda49
Muizzuddin-github <muizzuddin334@gmail.com>
2025-08-29 14:18:25 +0700

    fix welcome

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -4,15 +4,15 @@ import Link from "next/link";
 const Welcome = () => {
   return (
     <section className="mb-40">
-      <section className="flex justify-center flex-wrap md:flex-nowrap w-full md:w-3/4 m-auto px-10 md:px-0">
+      <section className="flex justify-center flex-wrap md:flex-nowrap w-full md:w-4/5 m-auto px-10 md:px-0">
         <section className="w-full flex justify-end py-10 md:py-0">
-          <section className="bg-[url('/house.jpeg')] w-full bg-cover bg-no-repeat bg-center h-[30rem] -top-10 relative hidden md:block">
+          <section className="bg-[url('/house.jpeg')] w-full bg-cover bg-no-repeat bg-center h-[40rem] -top-10 relative hidden md:block">
             <Image
               src="/kamarwelcome.jpg"
               width={300} // ubah sesuai kebutuhan
-              height={300} // ubah sesuai kebutuhan
+              height={500} // ubah sesuai kebutuhan
               alt="Living room"
-              className="absolute -bottom-40 right-0"
+              className="absolute -bottom-[9.5rem] right-0 h-[18rem]"
             />
           </section>
 
@@ -27,7 +27,7 @@ const Welcome = () => {
             <Image
               src="/kamarwelcome2.jpg"
               width={800} // ubah sesuai kebutuhan
-              height={300} // ubah sesuai kebutuhan
+              height={800} // ubah sesuai kebutuhan
               alt="Living room"
               className="m-auto"
             />
@@ -37,11 +37,18 @@ const Welcome = () => {
         <section className="w-full bg-gray-300 px-14 pr-5 py-10 h-fit">
           <section>
             <h2 className="font-semibold text-4xl">Selamat Datang</h2>
-            <p className="text-sm font-medium">Tentang Kami</p>
+            <p className="font-medium tracking-wider text-lg mt-4">
+              Tentang Kami
+            </p>
           </section>
 
-          <section className="mt-14">
-            <p className="font-semibold text-base leading-7">
+          <section className="mt-16">
+            <p className="font-medium text-xl leading-7">
+              Pondok Jepun, Nyaman Seperti di Rumah Sendiri Strategis, Tenang,
+              dan Dekat ke Semua Destinasi
+            </p>
+
+            <p className="font-semibold text-base leading-7 mt-14">
               Nikmati pengalaman menginap yang nyaman dan tenang di Guest House
               Pondok Jepun, sebuah penginapan bergaya homy yang terletak
               strategis di jantung kota Banyuwangi. Dikelilingi oleh pesona alam
@@ -50,13 +57,13 @@ const Welcome = () => {
               bisnis Anda.
             </p>
 
-            <section className="mt-12 flex flex-col gap-5 justify-center items-start">
+            <section className="mt-16 flex flex-col gap-10 justify-center items-start">
               <section className="flex items-center gap-3 w-fit">
                 <section className="w-7 bg-green-600 h-7 text-white rounded-full flex justify-center items-center">
                   <i className="fa-solid fa-check text-sm"></i>
                 </section>
 
-                <p className="text-sm">
+                <p className="text-base font-medium">
                   Menyuguhkan kamar yang nyaman, bersih, dan menenangkan.
                 </p>
               </section>
@@ -65,7 +72,7 @@ const Welcome = () => {
                   <i className="fa-solid fa-check text-sm"></i>
                 </section>
 
-                <p className="text-sm">
+                <p className="text-base font-medium">
                   Kamar utama nyaman dengan AC, TV, dan water heater.
                 </p>
               </section>
