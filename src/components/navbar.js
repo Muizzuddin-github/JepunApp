fd269f1c19f6a3ff8233672bc5520a7b68b0e894
Muizzuddin-github <muizzuddin334@gmail.com>
2025-08-29 21:27:51 +0700

    fix navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
-
+"use client";
+import { useState } from "react";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { useRef } from "react";
 
 export default function Navbar() {
+  const [showHide, setShowHide] = useState(false);
+  const pathname = usePathname();
+  const ulMenu = useRef();
+
   return (
     <nav className="bg-coklat_muda">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 md:justify-center">
@@ -20,9 +27,9 @@ export default function Navbar() {
           </span>
         </Link>
         <button
-          data-collapse-toggle="navbar-default"
+          onClick={() => setShowHide(!showHide)}
           type="button"
-          className="cursor-pointer inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none  dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="cursor-pointer inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-coklat hover:text-coklat_muda rounded-lg md:hidden hover:bg-coklat_tua focus:outline-none  "
           aria-controls="navbar-default"
           aria-expanded="false"
         >
@@ -43,48 +50,94 @@ export default function Navbar() {
             />
           </svg>
         </button>
-        <div className="w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4   rounded-lg bg-coklat_muda md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-coklat_muda">
+
+        <div
+          className={`w-full md:block md:w-auto ${showHide ? "" : "hidden"}`}
+        >
+          <ul
+            ref={ulMenu}
+            className="font-medium flex flex-col p-4 md:p-0 mt-4   rounded-lg bg-coklat_muda md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-coklat_muda"
+          >
             <li>
               <Link
                 href="/"
-                className="block py-2 px-3 text-coklat rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-coklat_tua md:p-0"
+                className="block py-2 px-3 text-coklat rounded-sm p-10 md:hover:bg-transparent md:border-0 md:hover:text-coklat_tua md:p-0"
                 aria-current="page"
               >
-                Home
+                <p
+                  className={`text-coklat border-b-2  ${
+                    pathname === "/"
+                      ? "  border-b-coklat"
+                      : "border-b-coklat_muda hover:border-b-coklat"
+                  } `}
+                >
+                  Home
+                </p>
               </Link>
             </li>
             <li>
-              <a
+              <Link
                 href="#"
-                className=" block py-2 px-3 text-coklat rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-coklat_tua md:p-0 "
+                className=" block py-2 px-3 text-coklat rounded-sm md:hover:bg-transparent md:border-0 md:hover:text-coklat_tua md:p-0 "
               >
-                About
-              </a>
+                <p
+                  className={`text-coklat border-b-2  ${
+                    pathname === "/about"
+                      ? "border-b-coklat"
+                      : "border-b-coklat_muda hover:border-b-coklat"
+                  } `}
+                >
+                  About
+                </p>
+              </Link>
             </li>
             <li>
               <Link
                 href="/service-fasilitas"
-                className="block py-2 px-3 text-coklat rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-coklat_tua md:p-0 "
+                className="block py-2 px-3 text-coklat rounded-sm md:hover:bg-transparent md:border-0 md:hover:text-coklat_tua md:p-0 "
               >
-                Services & Facilities
+                <p
+                  className={`text-coklat border-b-2 ${
+                    pathname === "/service-fasilitas"
+                      ? "border-b-coklat"
+                      : "border-b-coklat_muda hover:border-b-coklat"
+                  } `}
+                >
+                  Services & Facilities
+                </p>
               </Link>
             </li>
             <li>
-              <a
+              <Link
                 href="#"
-                className="block py-2 px-3 text-coklat rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-coklat_tua md:p-0 "
+                className="block py-2 px-3 text-coklat rounded-sm md:hover:bg-transparent md:border-0 md:hover:text-coklat_tua md:p-0 "
               >
-                Pricing
-              </a>
+                <p
+                  className={`text-coklat border-b-2  ${
+                    pathname === "/pricing"
+                      ? "border-b-coklat"
+                      : "border-b-coklat_muda hover:border-b-coklat"
+                  } `}
+                >
+                  Pricing
+                </p>
+              </Link>
             </li>
             <li>
-              <a
+              <Link
                 href="#"
-                className="block py-2 px-3 text-coklat rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-coklat_tua md:p-0 "
+                className="block py-2 px-3 text-coklat rounded-sm md:hover:bg-transparent md:border-0 md:hover:text-coklat_tua md:p-0 "
               >
-                Contact
-              </a>
+                <p
+                  className={`text-coklat border-b-2 ${
+                    pathname === "/contact"
+                      ? "border-b-coklat"
+                      : "border-b-coklat_muda hover:border-b-coklat"
+                  } `}
+                >
+                  Contact
+                </p>
+              </Link>
             </li>
           </ul>
         </div>
