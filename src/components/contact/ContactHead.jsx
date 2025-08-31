ee14788ab57cc4e2937e3d32ca434adb6b122724
Muizzuddin-github <muizzuddin334@gmail.com>
2025-08-31 16:15:06 +0700

    ok

--- src/components/contact/ContactHead.jsx
+++ src/components/contact/ContactHead.jsx
@@ -1,16 +1,13 @@
-
 const ContactHead = () => {
-    return (
-        <section
-            className="bg-cover h-[40vh] lg:h-[55vh] bg-center bg-no-repeat bg-[url('/house.jpeg')] bg-black/50  bg-blend-multiply content-center">
-            <div
-                className=" p-10 text-center ">
-                <h1 className="nicolas mb-4 md:text-[52px]  text-[32px] tracking-tight leading-none text-white   drop-shadow-lg">
-                    Contact Page
-                </h1>
-                <hr className="h-0.5  w-28 bg-white  rounded-full m-auto text-white" />
-            </div>
-        </section>
-    );
+  return (
+    <section className="bg-cover h-[40vh] lg:h-[55vh] bg-center bg-no-repeat bg-[url('/house.jpeg')] bg-black/50  bg-blend-multiply content-center will-change-transform bg-fixed">
+      <div className=" p-10 text-center ">
+        <h1 className="nicolas mb-4 md:text-[52px]  text-[32px] tracking-tight leading-none text-white   drop-shadow-lg">
+          Contact Page
+        </h1>
+        <hr className="h-0.5  w-28 bg-white  rounded-full m-auto text-white" />
+      </div>
+    </section>
+  );
 };
 export default ContactHead;
